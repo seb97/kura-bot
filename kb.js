c33e31b7dbfe6e9191eae79e74829b6d465fce60
kurasoooooooooooooo <35342616+kurabot@users.noreply.github.com>
2019-01-22 22:57:01 +0100

    Update kb.js

--- kb.js
+++ kb.js
@@ -75,7 +75,7 @@ return "#" + math.floor(math.random()*16777215).toString(16);
 if(message.content.startsWith(prefix + "cr")) {
 if (!message.member.hasPermission("MANAGE_ROLES","ADMINISTRATOR")) return message.reply("tu n'a pas le droit d'utiliser cette commande");
 if(!args.length) return message.reply("definire le nom du role").
-message.guild.createRole({
+message.guild.role.create({
 data: {
 name: `${args}`,
 color: couleur(),
